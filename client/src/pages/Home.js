48039ae26636e650310b67f3865db6874f4beedd
Medici <107539009+taholton748@users.noreply.github.com>
2023-01-07 00:50:43 -0700

    added logo to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,11 @@
+import logo from '../components/Images/PlaceBook.png';
+import '../App.css';
+
 export default function Home() {
   return (
-    <div>
-      <h2>Home</h2>
+    <div style={{ backgroundImage: 'url(./components/Images/home-background.jpeg)' }}>
+      <h2 className="homeTitle">Where Travelers Connect and Share</h2>
+      <img src={logo} alt="logo" className="App-logo" />
     </div>
   );
 }
