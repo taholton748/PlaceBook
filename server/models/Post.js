b75c1950ebde714391fcf1ddf301557437e4efe6
Paul Todd <ptodd00@gmail.com>
2023-01-07 15:58:36 -0500

    update post model, add comment model

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,33 +1,42 @@
 const mongoose = require('mongoose');
-
 const { Schema } = mongoose;
-
-// TODO: add comment model so the below can be imported
-// const commentSchema = require('./Comment');
-
+why
 const dateFormat = require('../utils/dateFormat');
 
 const postSchema = new Schema({
-  title: { type: String, required: 'You must provide a title!' },
-  description: { type: String, required: 'You must add a description!' },
-  photos: { type: String },
-  location: { type: String },
-  postLikes: [
-    {
-      userId: { type: String, required: true },
-    },
-  ],
-  // need to add comment model so this can be used
-  // comments: [commentSchema],
+  title: {
+    type: String,
+    required: 'You must provide a title!'
+  },
+  username: {
+    type: String,
+    default: req.user.username,
+    required: 'You must add a description!'
+  },
+  discreption: {
+    type: String,
+    required: true
+  },
+  photo: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  }],
+  postLikes: [{
+    type: Number,
+    ref: 'User'
+  }],
   createdAt: {
     type: Date,
     default: Date.now,
     get: timestamp => dateFormat(timestamp)
-  },
-  // user: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: 'User',
-  // },
+
+  }
 });
 
 const Post = mongoose.model('Post', postSchema);
