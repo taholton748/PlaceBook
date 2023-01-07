743802acf667668971453913be2fb458b5b0ddbb
Medici <107539009+taholton748@users.noreply.github.com>
2023-01-07 03:17:03 -0700

    added travel quotes to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,7 +5,9 @@ export default function Home() {
   return (
     <div className="homeBody">
       <h1 className="homeTextLeft">Where Travelers Connect and Share Their Journeys</h1>
+      <p className="top-quote">Not all those who wander are lost</p>
       <img src={logo} alt="logo" className="App-logo" />
+      <p className="bottom-quote">Jobs fill your pockets, adventues fill your soul</p>
       <h1 className="homeTextCenter">Wanderlust Social Media</h1>
     </div>
   );
