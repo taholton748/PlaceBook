116e619cd13b6c6bba7bd945e098f6175972ba44
Paul Todd <ptodd00@gmail.com>
2023-01-10 17:00:42 -0500

    new post modal, with cloud widget

---
+++ client/src/components/NewPost/index.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { Button, Modal, Form, Rating } from 'semantic-ui-react';
+import UploadWidget from '../UploadWidget/index';
+
+function CreatePostModal() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Modal
+      open={open}
+      trigger={<Button onClick={() => setOpen(true)}>Create New Post</Button>}
+      onClose={() => setOpen(false)}
+    >
+      <Modal.Header>Create New Post</Modal.Header>
+      <Modal.Content>
+        <Form>
+        <Form.Field>
+            <label>Photo</label>
+            <UploadWidget />
+          </Form.Field>
+          <Form.Field>
+            <label>Description</label>
+            <textarea id="description" name="description" />
+          </Form.Field>
+          <Form.Field>
+            <label>Location</label>
+            <input id="location" name="location" />
+          </Form.Field>
+          <Button type="submit" onClick={handleSubmit}>Submit</Button>
+          <Form.Field>
+            <label>Rating</label>
+            <Rating icon="star" defaultRating={0} maxRating={5} />
+          </Form.Field>
+        </Form>
+      </Modal.Content>
+    </Modal>
+  );
+}
+
+function handleSubmit() {
+  // Get the form data
+  const description = document.getElementById('description').value;
+  const photo = document.getElementById('photo').files[0];
+  const location = document.getElementById('location').value;
+
+  // Create a new post object
+  const newPost = new Post({
+    username: 'JohnDoe', // replace with actual username
+    description: description,
+    location: location,
+  });
+
+  // Save the new post to the database
+  newPost.save((error) => {
+    if (error) {
+      // handle error
+    } else {
+      // post saved successfully, close the modal
+      setOpen(false);
+    }
+  });
+}
+
+export default CreatePostModal;
