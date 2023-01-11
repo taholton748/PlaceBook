82b6ace461f038df3e0f9e2c4c552b3d3ee86ee8
Jonathan Keku <jkeku@fetrigon.com>
2023-01-10 23:25:36 -0500

    Placeholder profiles added to friendslist on profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -17,9 +17,58 @@ export default function AutoLayoutExample() {
             <p>Harvard University</p>
           </Row>
           <Row className="card">
-            <img src={logo} alt="logo" style={{ width: '40%' }} className="App-logo-small" />
             <h1>Friends</h1>
             <p className="title">Friends Here</p>
+            <div className="event">
+              <div className="label">
+                <img src="https://react.semantic-ui.com/images/avatar/small/elliot.jpg" alt="prop" />
+              </div>
+              <div className="content">
+                <div className="summary">
+                  <a href="mailto:example@example.com" className="user">Elliot Fu</a>
+                </div>
+              </div>
+            </div>
+            <div className="event">
+              <div className="label">
+                <img src="https://react.semantic-ui.com/images/avatar/small/elliot.jpg" alt="prop" />
+              </div>
+              <div className="content">
+                <div className="summary">
+                  <a href="mailto:example@example.com" className="user">Elliot Fu</a>
+                </div>
+              </div>
+            </div>
+            <div className="event">
+              <div className="label">
+                <img src="https://react.semantic-ui.com/images/avatar/small/elliot.jpg" alt="prop" />
+              </div>
+              <div className="content">
+                <div className="summary">
+                  <a href="mailto:example@example.com" className="user">Elliot Fu</a>
+                </div>
+              </div>
+            </div>
+            <div className="event">
+              <div className="label">
+                <img src="https://react.semantic-ui.com/images/avatar/small/elliot.jpg" alt="prop" />
+              </div>
+              <div className="content">
+                <div className="summary">
+                  <a href="mailto:example@example.com" className="user">Elliot Fu</a>
+                </div>
+              </div>
+            </div>
+            <div className="event">
+              <div className="label">
+                <img src="https://react.semantic-ui.com/images/avatar/small/elliot.jpg" alt="prop" />
+              </div>
+              <div className="content">
+                <div className="summary">
+                  <a href="mailto:example@example.com" className="user">Elliot Fu</a>
+                </div>
+              </div>
+            </div>
           </Row>
         </Col>
         <Col className="card"><Feed /></Col>
