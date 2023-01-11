ee72804d221f9849de77d95dd2a6ecaf64eb32fd
Jonathan Keku <jkeku@fetrigon.com>
2023-01-10 21:51:59 -0500

    Switch to bootstrap grid layout

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,52 +6,26 @@ import '../App.css';
 export default function Profile() {
   return (
     <div className="background">
-      <div className="profile">
-        <div className="card">
-          <img src={logo} alt="logo" style={{ width: '40%' }} className="App-logo-small" />
-          <h1>John Doe</h1>
-          <p className="title">CEO & Founder, Example</p>
-          <p>Harvard University</p>
-        </div>
-      </div>
-      <div className="feed">
-        <div className="card">
-          <h1>feed</h1>
-        </div>
-        <div className="card">
-          <p className="title">CEO & Founder, Example</p>
-        </div>
-        <div className="card">
-          <p>Harvard University</p>
-        </div>
-        <div className="card">
-          <p>Harvard University</p>
-        </div>
-        <div className="card">
-          <p>Harvard University</p>
-        </div>
-        <div className="card">
-          <p>Harvard University</p>
-        </div>
-        <div className="card">
-          <p>Harvard University</p>
-        </div>
-      </div>
-      <div className="friends">
-        <div className="card">
-          <h1>friends</h1>
-        </div>
-        <div className="card">
-          <img src={logo} alt="logo" style={{ width: '20%' }} className="App-logo-small" />
-          <h1>John Doe</h1>
-          <p className="title">CEO & Founder, Example</p>
-          <p>Harvard University</p>
-        </div>
-        <div className="card">
-          <img src={logo} alt="logo" style={{ width: '20%' }} className="App-logo-small" />
-          <h1>Jane Doe</h1>
-          <p className="title">CEO & Founder, Example</p>
-          <p>Harvard University</p>
+      <div className="container">
+        <div className="row"/* I */>
+          <div className="col-sm-3"/* profile */>
+            <div className="card">
+              <img src={logo} alt="logo" style={{ width: '40%' }} className="App-logo-small" />
+              <h1>John Doe</h1>
+              <p className="title">CEO & Founder, Example</p>
+              <p>Harvard University</p>
+            </div>
+          </div>
+          <div className="col-sm-6"/* feed */>
+            <div className="card">
+              <h1>feed</h1>
+            </div>
+          </div>
+          <div className="col-sm-3"/* {friend} */>
+            <div className="card">
+              <h1>friends</h1>
+            </div>
+          </div>
         </div>
       </div>
     </div>
