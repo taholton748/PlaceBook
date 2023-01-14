9fbc32d6168d253b1425dfe9d74b9f76610af14f
Alex Defrese <aidandefrese@gmail.com>
2023-01-13 19:38:51 -0500

    attempted to update server for Heroku deployment

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const { authMiddleware } = require('./utils/auth');
 
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
