5c90eab29f521b08d496a9ab0129e0496ed9c478
Alex Defrese <aidandefrese@gmail.com>
2023-01-18 18:47:01 -0500

    updated PORT to try heroku deployment

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ const startApolloServer = async () => {
   server.applyMiddleware({ app });
 
   db.once('open', () => {
-    app.listen(PORT, () => {
+    app.listen(process.env.PORT || 3001, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     });
