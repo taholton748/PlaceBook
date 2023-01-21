33cfd289a7ecc8a36bf1ee7f18e5d5fb3c7c769f
DTGuy <tguy5837@gmail.com>
2023-01-21 02:03:28 -0500

    working on PostForm.js

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -7,20 +7,16 @@ import PostForm from '../components/PostForm';
 
 import { QUERY_POSTS, QUERY_CURRENT_USER } from '../graphql/queries';
 
-import CurrentUserContextProvider from '../context/currentUser';
-
 export default function Dashboard() {
   // use useQuery hook to make query request
   const { loading, error, data } = useQuery(QUERY_POSTS);
   // console.log('Error ---', JSON.stringify(error, null, 2));
-  console.log('Data ---', data);
 
   // use object destructuring to extract `data` from the `useQuery` Hook's response and
   // rename it `userData` to be more descriptive
   const { data: userData } = useQuery(QUERY_CURRENT_USER);
 
   const posts = data?.getPosts || [];
-  console.log('Posts ---', posts);
 
   return (
     <div>
